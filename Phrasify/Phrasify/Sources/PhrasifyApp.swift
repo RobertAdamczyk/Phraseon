c4c008aa6e5efaeb19b1620f267962bba45ad305
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2023-12-11 21:20:29 +0100

    first commit

---
+++ Phrasify/Phrasify/Sources/PhrasifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhrasifyApp.swift
+//  Phrasify
+//
+//  Created by Robert Adamczyk on 11.12.23.
+//
+
+import SwiftUI
+
+@main
+struct PhrasifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
