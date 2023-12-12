c90f99ab197913ca5e5e0951c28c9405b352543f
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2023-12-12 20:46:16 +0100

    added mock coordinator to use preview

--- Phrasify/Phrasify/Preview Content/MockCoordinator.swift
+++ Phrasify/Phrasify/Preview Content/MockCoordinator.swift
@@ -5,4 +5,17 @@
 //  Created by Robert Adamczyk on 12.12.23.
 //
 
-import Foundation
+import SwiftUI
+
+#if DEBUG
+
+final class MockCoordinator: Coordinator {
+
+    var dependencies: Dependencies = .init(repository: "")
+
+    func createRootView() -> AnyView {
+        .init(EmptyView())
+    }
+}
+
+#endif
