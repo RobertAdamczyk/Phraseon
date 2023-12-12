4f3a1bfe98bc85357fecfd336b47ae473df91066
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2023-12-12 21:45:43 +0100

    add app divider

---
+++ Phrasify/Phrasify/Sources/Basic/AppDivider.swift
@@ -0,0 +1,17 @@
+//
+//  AppDivider.swift
+//  Phrasify
+//
+//  Created by Robert Adamczyk on 12.12.23.
+//
+
+import SwiftUI
+
+struct AppDivider: View {
+
+    var body: some View {
+        Rectangle()
+            .frame(height: 2)
+            .foregroundStyle(appColor(.lightGray))
+    }
+}
