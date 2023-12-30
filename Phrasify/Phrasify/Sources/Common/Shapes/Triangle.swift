dcffa9400c8f908306ecf23c414c5a973fe20698
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2023-12-30 23:22:14 +0100

    add triangle shape

---
+++ Phrasify/Phrasify/Sources/Common/Shapes/Triangle.swift
@@ -0,0 +1,21 @@
+//
+//  Triangle.swift
+//  Phrasify
+//
+//  Created by Robert Adamczyk on 30.12.23.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+
+        return path
+    }
+}
--- Phrasify/Phrasify/Sources/Scenes/Home/ProjectDetail/ProjectDetailView+KeyRow.swift
+++ Phrasify/Phrasify/Sources/Scenes/Home/ProjectDetail/ProjectDetailView+KeyRow.swift
@@ -52,19 +52,6 @@ extension ProjectDetailView {
     }
 }
 
-struct Triangle: Shape {
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-
-        path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
-
-        return path
-    }
-}
-
 #Preview {
     ZStack {
         appColor(.black)
