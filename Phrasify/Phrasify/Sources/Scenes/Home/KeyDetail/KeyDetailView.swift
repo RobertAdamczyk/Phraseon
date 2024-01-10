af7610795be934f3d2ea704efd716dd9a4280721
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-01-10 17:13:46 +0100

    key detail view ui

--- Phrasify/Phrasify/Sources/Scenes/Home/KeyDetail/KeyDetailView.swift
+++ Phrasify/Phrasify/Sources/Scenes/Home/KeyDetail/KeyDetailView.swift
@@ -12,78 +12,79 @@ struct KeyDetailView: View {
     @ObservedObject var viewModel: KeyDetailViewModel
 
     var body: some View {
-
-        ZStack {
-            ScrollView(showsIndicators: false) {
-                VStack(spacing: 32) {
-                    ForEach(viewModel.project.languages.reversed(), id: \.self) { language in
-                        if let translation = viewModel.key.translation[language.rawValue] {
-                            VStack(alignment: .leading, spacing: 16) {
-                                HStack(spacing: 16) {
-                                    Image(language.rawValue)
-                                        .resizable()
-                                        .frame(width: 24, height: 24)
-                                        .padding(2)
-                                        .background {
-                                            Circle()
-                                                .fill(appColor(.white))
-                                        }
-                                    Text(language.localizedTitle + (language == viewModel.project.baseLanguage ? " (Base)" : ""))
-                                        .apply(.medium, size: .L, color: .lightGray)
-                                }
-                                .padding(.vertical, 6)
-                                .padding(.horizontal, 12)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 8)
-                                        .foregroundStyle(appColor(.darkGray))
-                                }
-                                Text(translation)
-                                    .apply(.regular, size: .S, color: .white)
-                                    .multilineTextAlignment(.leading)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .padding(12)
-                                    .background {
-                                        RoundedRectangle(cornerRadius: 16)
-                                            .foregroundStyle(appColor(.darkGray))
-                                    }
-                            }
-                        } else {
-                            VStack(alignment: .leading, spacing: 16) {
-                                HStack(spacing: 16) {
-                                    Image(language.rawValue)
-                                        .resizable()
-                                        .frame(width: 24, height: 24)
-                                        .padding(2)
-                                        .background {
-                                            Circle()
-                                                .fill(appColor(.white))
-                                        }
-                                    Text(language.localizedTitle)
-                                        .apply(.medium, size: .L, color: .lightGray)
-                                }
-                                .padding(.vertical, 6)
-                                .padding(.horizontal, 12)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 8)
-                                        .foregroundStyle(appColor(.darkGray))
-                                }
-                                Text("No translation into the language.")
-                                    .apply(.regular, size: .S, color: .white)
-                                    .multilineTextAlignment(.leading)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .padding(12)
-                                    .background {
-                                        RoundedRectangle(cornerRadius: 16)
-                                            .foregroundStyle(appColor(.darkGray))
-                                    }
-                            }
-                        }
+        ScrollView(showsIndicators: false) {
+            VStack(spacing: 32) {
+                ForEach(viewModel.project.languages.reversed(), id: \.self) { language in
+                    if let translation = viewModel.key.translation[language.rawValue] {
+                        makeLanguageRow(for: language, translation: translation)
+                    } else {
+                        makeLanguageRow(for: language, translation: nil)
                     }
                 }
-                .padding(16)
             }
+            .padding(16)
         }
         .navigationTitle(viewModel.key.id ?? "Phrase Detail")
+    }
+
+    private func makeLanguageRow(for language: Language, translation: String?) -> some View {
+        VStack(alignment: .leading, spacing: 16) {
+            HStack(spacing: 16) {
+                Image(language.rawValue)
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .padding(2)
+                    .background {
+                        Circle()
+                            .fill(appColor(.white))
+                    }
+                Text(language.localizedTitle)
+                    .apply(.medium, size: .L, color: .lightGray)
+            }
+            .padding(.vertical, 6)
+            .padding(.horizontal, 12)
+            .background {
+                RoundedRectangle(cornerRadius: 8)
+                    .foregroundStyle(appColor(.darkGray))
+            }
+            VStack(alignment: .trailing, spacing: 8) {
+                Text(translation ?? "No translation into the language.")
+                    .apply(.regular, size: .S, color: .white)
+                    .multilineTextAlignment(.leading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                HStack(spacing: 16) {
+                    editButton
+                    copyButton
+                }
+            }
+            .padding(12)
+            .background {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(appColor(.darkGray))
+            }
+        }
+    }
+
+    private var editButton: some View {
+        Button {
+            print("XD")
+        } label: {
+            Image(systemName: "square.and.pencil")
+                .resizable()
+                .frame(width: 22, height: 22)
+                .apply(.medium, size: .M, color: .paleOrange)
+        }
+    }
+
+    private var copyButton: some View {
+        Button {
+            print("XD")
+        } label: {
+            Image(systemName: "doc.on.doc")
+                .resizable()
+                .frame(width: 18, height: 22)
+                .apply(.medium, size: .M, color: .paleOrange)
+        }
     }
 }
 
