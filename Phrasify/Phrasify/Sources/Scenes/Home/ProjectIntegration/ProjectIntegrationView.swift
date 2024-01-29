c586c0ea49832c83e7fc0d5e18b424d9bf3f416b
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-01-29 20:57:21 +0100

    adjust ui to integration view

--- Phrasify/Phrasify/Sources/Scenes/Home/ProjectIntegration/ProjectIntegrationView.swift
+++ Phrasify/Phrasify/Sources/Scenes/Home/ProjectIntegration/ProjectIntegrationView.swift
@@ -16,42 +16,45 @@ struct ProjectIntegrationView: View {
             ScrollView(showsIndicators: false) {
                 VStack(alignment: .leading, spacing: 32) {
                     AppTitle(subtitle: "This script synchronizes phrase localizations by downloading translations from a Phrasify and saving them into your project directories.")
-                    AppDivider()
                     VStack(alignment: .leading, spacing: 16) {
                         Label("Pre-requisites", systemImage: "1.circle")
                             .apply(.medium, size: .M, color: .lightGray)
                         VStack(alignment: .leading, spacing: 16) {
-                            Text("- Ensure ") + Text("curl").bold() + Text(" and ") + Text("jq").bold() + Text(" are installed on your system.")
-                            Text("- Confirm network connectivity for accessing the project.")
+                            Text("Ensure ") + Text("curl").bold() + Text(" and ") + Text("jq").bold() + Text(" are installed on your system.")
                         }
                         .apply(.regular, size: .M, color: .white)
-                        .padding(.horizontal, 8)
+                        .padding(8)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .applyCellBackground()
                     }
-                    AppDivider()
                     VStack(alignment: .leading, spacing: 16) {
                         Label("Configure the Script", systemImage: "2.circle")
                             .apply(.medium, size: .M, color: .lightGray)
                         VStack(alignment: .leading, spacing: 16) {
-                            Text("- Open the script in a text editor.")
-                            Text("- Locate the line RELATIVE_PATH=\"/YOUR_PATH/TO_LOCALIZATIONS\"")
-                            Text("- Replace with the actual path where you want the localization files to be stored.")
+                            Text("Open the script in a text editor.")
+                            Text("Locate the line RELATIVE_PATH=\"/YOUR_PATH/TO_LOCALIZATIONS\"")
+                            Text("Replace with the actual path where you want the localization files to be stored.")
                         }
                         .apply(.regular, size: .M, color: .white)
-                        .padding(.horizontal, 8)
+                        .padding(8)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .applyCellBackground()
                     }
-                    AppDivider()
                     VStack(alignment: .leading, spacing: 16) {
                         Label("Running the Script", systemImage: "3.circle")
                             .apply(.medium, size: .M, color: .lightGray)
                         VStack(alignment: .leading, spacing: 16) {
-                            Text("- Open a terminal window")
-                            Text("- Navigate to the directory containing the script.")
-                            Text("- Execute the script by typing sh syncPhrases.sh and pressing Enter.")
-                            Text("- The script checks for network connectivity. If offline, it will exit without synchronizing.")
+                            Text("Open a terminal window")
+                            Text("Navigate to the directory containing the script.")
+                            Text("Execute the script by typing \"sh syncPhrases.sh\" and pressing Enter.")
+                            Text("The script checks for network connectivity. If offline, it will exit without synchronizing.")
                         }
                         .apply(.regular, size: .M, color: .white)
-                        .padding(.horizontal, 8)
+                        .padding(8)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .applyCellBackground()
                     }
+
                 }
                 .padding(16)
             }
@@ -67,6 +70,7 @@ struct ProjectIntegrationView: View {
         .navigationTitle("Integration")
         .fileExporter(isPresented: $viewModel.shouldShowExportSheet, document: viewModel.syncScriptFile,
                       contentType: .shellScript, defaultFilename: viewModel.defaultFilename, onCompletion: viewModel.onExportCompletion)
+        .applyViewBackground()
     }
 }
 
