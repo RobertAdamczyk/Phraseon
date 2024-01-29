ba9e77e073832481e1319ab597fb1b8f58d85a6f
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-01-29 21:28:56 +0100

    change color for activity view

--- Phrasify/Phrasify/Sources/Common/ActivityView/ActivityView.swift
+++ Phrasify/Phrasify/Sources/Common/ActivityView/ActivityView.swift
@@ -17,6 +17,12 @@ extension View {
 private struct ActivityViewModifier: ViewModifier {
     let isLoading: Bool
 
+    /// A keypath that finds the color value for all `Fill 1` nodes.
+    let fillKeypath = AnimationKeypath(keypath: "**.Fill 1.Color")
+    /// A Color Value provider that returns the white color.
+    let greyValueProvider = ColorValueProvider(LottieColor(r: 1, g: 1, b: 1, a: 1))
+
+
     func body(content: Content) -> some View {
         ZStack {
             content
@@ -25,12 +31,10 @@ private struct ActivityViewModifier: ViewModifier {
                     appColor(.black).opacity(0.5)
                     LottieView(animation: .named("buttonLoadingAnimation"))
                         .playing(loopMode: .loop)
+                        .valueProvider(greyValueProvider, for: fillKeypath)
                         .scaleEffect(2)
                         .frame(width: 160, height: 160)
-                        .background {
-                            RoundedRectangle(cornerRadius: 16)
-                                .fill(appColor(.lightGray))
-                        }
+                        .applyCellBackground()
                 }
                 .ignoresSafeArea()
             }
