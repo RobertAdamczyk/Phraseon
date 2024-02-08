fcd782d3865046de1ee6233795dfed5a4bcfee6b
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-02-08 22:28:42 +0100

    Check if the file was created in pre build script

--- Phrasify/ci_scripts/ci_pre_xcodebuild.sh
+++ Phrasify/ci_scripts/ci_pre_xcodebuild.sh
@@ -13,6 +13,14 @@ echo $ALGOLIA_ADMIN_KEY
 # Write a JSON File containing all the environment variables and secrets.
 printf "{\"ALGOLIA_APP_ID\":\"%s\",\"ALGOLIA_ADMIN_KEY\":\"%s\"}" "$ALGOLIA_APP_ID" "$ALGOLIA_ADMIN_KEY" >> ../Phraseon/Phraseon_InHouse/Resources/secrets.json
 
+# Check if the file was created
+if test -f "../Phraseon/Phraseon_InHouse/Resources/secrets.json"; then
+    echo "The secrets.json file has been successfully created."
+else
+    echo "Failed to create the secrets.json file."
+    exit 1  # Optional: exit with an error if the file doesn't exist
+fi
+
 echo "Wrote Secrets.json file."
 
 echo "InHouse: PRE-Xcode Build is DONE .... "
