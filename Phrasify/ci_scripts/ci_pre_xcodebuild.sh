5994dd14135e8b7bcef478de421ce247a9d6af3b
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-02-08 23:08:27 +0100

    add secrets for live builds

--- Phrasify/ci_scripts/ci_pre_xcodebuild.sh
+++ Phrasify/ci_scripts/ci_pre_xcodebuild.sh
@@ -5,25 +5,20 @@
 #
 #  Created by Robert Adamczyk on 08.02.24.
 #
-echo "InHouse: PRE-Xcode Build is activated .... "
-echo $CI_XCODE_SCHEME
-echo $CI_PRODUCT_PLATFORM
-echo $CI_PRODUCT
-echo $CI_XCODE_PROJECT
+echo "PRE-Xcode Build is activated .... "
 
 # Write a JSON File containing all the environment variables and secrets.
-printf "{\"ALGOLIA_APP_ID\":\"%s\",\"ALGOLIA_ADMIN_KEY\":\"%s\"}" "$ALGOLIA_APP_ID" "$ALGOLIA_ADMIN_KEY" >> ../Phraseon_InHouse/Resources/secrets.json
+filePath="../$CI_PRODUCT/Resources/secrets.json"
+printf "{\"ALGOLIA_APP_ID\":\"%s\",\"ALGOLIA_ADMIN_KEY\":\"%s\"}" "$ALGOLIA_APP_ID" "$ALGOLIA_ADMIN_KEY" >> "$filePath"
 
 # Check if the file was created
-if test -f "../Phraseon_InHouse/Resources/secrets.json"; then
+if test -f "$filePath"; then
     echo "The secrets.json file has been successfully created."
 else
     echo "Failed to create the secrets.json file."
     exit 1  # Optional: exit with an error if the file doesn't exist
 fi
 
-echo "Wrote Secrets.json file."
-
-echo "InHouse: PRE-Xcode Build is DONE .... "
+echo "PRE-Xcode Build is DONE .... "
 
 exit 0
