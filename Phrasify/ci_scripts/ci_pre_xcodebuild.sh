e258dfdf558246cf55fe74ea921d5c401630b031
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-02-08 22:18:15 +0100

    pre build script test environment variables

--- Phrasify/ci_scripts/ci_pre_xcodebuild.sh
+++ Phrasify/ci_scripts/ci_pre_xcodebuild.sh
@@ -6,10 +6,13 @@
 #  Created by Robert Adamczyk on 08.02.24.
 #
 echo "InHouse: PRE-Xcode Build is activated .... "
+echo $CI_WORKSPACE
+echo $ALGOLIA_APP_ID
+echo $ALGOLIA_ADMIN_KEY
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
-cd $CI_WORKSPACE/ci_scripts || exit 1
+cd ci_scripts || exit 1
 
 # Write a JSON File containing all the environment variables and secrets.
 printf "{\"ALGOLIA_APP_ID\":\"%s\",\"ALGOLIA_ADMIN_KEY\":\"%s\"}" "$ALGOLIA_APP_ID" "$ALGOLIA_ADMIN_KEY" >> ../Phraseon/Phraseon_InHouse/Resources/secrets.json
