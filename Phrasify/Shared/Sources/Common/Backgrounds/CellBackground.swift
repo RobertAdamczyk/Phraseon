13fee0c2b53d5527d9dc3a15de5ae839ad248bde
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-03-19 22:53:44 +0100

    CellBackground cornerRadius from outside

--- Phrasify/Shared/Sources/Common/Backgrounds/CellBackground.swift
+++ Phrasify/Shared/Sources/Common/Backgrounds/CellBackground.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 fileprivate struct CellBackground: ViewModifier {
 
+    let cornerRadius: CGFloat
+
     private let veryDarkGray: Color = .init(red: 39/255, green: 39/255, blue: 39/255)
     private let darkGray: Color = .init(red: 46/255, green: 46/255, blue: 46/255)
     private let gray: Color = .init(red: 70/255, green: 70/255, blue: 70/255)
@@ -18,17 +20,17 @@ fileprivate struct CellBackground: ViewModifier {
             .background {
                 LinearGradient(gradient: Gradient(colors: [veryDarkGray, darkGray]), startPoint: .bottom, endPoint: .top)
                 .overlay {
-                    RoundedRectangle(cornerRadius: 8)
+                    RoundedRectangle(cornerRadius: cornerRadius)
                         .stroke(lineWidth: 2)
                         .fill(LinearGradient(gradient: Gradient(colors: [veryDarkGray, gray]), startPoint: .bottom, endPoint: .top))
                 }
             }
-            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .clipShape(RoundedRectangle(cornerRadius: cornerRadius))
     }
 }
 
 extension View {
-    func applyCellBackground() -> some View {
-        modifier(CellBackground())
+    func applyCellBackground(cornerRadius: CGFloat = 8) -> some View {
+        modifier(CellBackground(cornerRadius: cornerRadius))
     }
 }
