b4307d0b7a06c28292444203a72a902832712782
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-04-01 23:37:38 +0200

    add FirebaseSecrets.plist for debug builds

--- Phrasify/Shared/Sources/PhraseonApp.swift
+++ Phrasify/Shared/Sources/PhraseonApp.swift
@@ -17,7 +17,7 @@ struct PhraseonApp: App {
         guard let target = Target(rawValue: Bundle.main.bundleURL.lastPathComponent) else { fatalError("Target not found !") }
         TargetConfiguration.shared.setup(target: target)
         Secrets.shared.setup(path: Bundle.main.path(forResource: "secrets", ofType: "json"))
-        FirebaseApp.configure()
+        configureFirebase()
     }
 
     var body: some Scene {
@@ -29,5 +29,18 @@ struct PhraseonApp: App {
                     GIDSignIn.sharedInstance.handle(url)
                 })
         }
+    }
+}
+
+extension PhraseonApp {
+
+    func configureFirebase() {
+        #if DEBUG
+        let filePath = Bundle.main.path(forResource: "FirebaseSecrets", ofType: "plist")
+        #else
+        let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")
+        #endif
+        guard let filePath, let options = FirebaseOptions(contentsOfFile: filePath) else { fatalError("Firebase configure error.") }
+        FirebaseApp.configure(options: options)
     }
 }
