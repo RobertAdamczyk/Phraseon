3fee5bcbfde11161b7faa34a38e35e7ce2c04b8c
Robert Adamczyk FFW <robert.adamczyk@ffwagency.com>
2024-04-20 12:14:59 +0200

    move role sortIndex to spm model

--- Phrasify/Modules/Model/Sources/Model/Role.swift
+++ Phrasify/Modules/Model/Sources/Model/Role.swift
@@ -14,4 +14,14 @@ public enum Role: String, Codable, Hashable, CaseIterable {
     case developer = "DEVELOPER"
     case viewer = "VIEWER"
     case marketing = "MARKETING"
+
+    public var sortIndex: Int {
+        switch self {
+        case .owner: return 0
+        case .admin: return 1
+        case .developer: return 2
+        case .marketing: return 3
+        case .viewer: return 4
+        }
+    }
 }
